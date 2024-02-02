e8b28d508f1ad841caa099a8eb18e6533fa9c5ad
Marika <113508344+MarikaGK@users.noreply.github.com>
2024-02-02 19:38:25 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="container">
       <h3>
@@ -23,7 +18,7 @@ function App() {
         </ul>
         <p className="text">
           I po omacku szukasz rozwiązań i poprawy zdrowia łykając coraz większe
-          ilości lekarstw
+          ilości lekarstefef
         </p>
         <p className="text">
           albo chcesz wyprzedzić czas i uniknąć tych wszystkich dolegliwości?
@@ -35,14 +30,367 @@ function App() {
         stanów zapalnych oraz poznać kuracje zdrowotne przeciwstarzeniowe (anty
         aging)
       </h4>
-<a href="https://www.facebook.com/dietoteka/reviews">Opinie o szkoleniach Izabeli Potrzebowskiej -Kniaziuk</a>
-<h5>Witaj w świecie, który sprawi, że będziesz wyglądać i czuć się młodziej</h5>
-<p>Chcemy przedstawić Ci najnowszy hit na rynku - <strong>niepowtarzalne szkolenie
-przeciwzapalne i przeciwstarzeniowe</strong>, które zrewolucjonizuje Twoje podejście do zdrowia!
-</p>
-<p>Nawet techniczni Panowie się odnajdują na naszych spotkaniach!</p>
-
-
+      <a
+        href="https://www.facebook.com/dietoteka/reviews"
+        target="_blank"
+        rel="noreferrer"
+      >
+        opinie o szkoleniach izabeli potrzebowskiej -kniaziuk
+      </a>
+      <h5>
+        Witaj w świecie, który sprawi, że będziesz wyglądać i czuć się młodziej
+      </h5>
+      <p>
+        Chcemy przedstawić Ci najnowszy hit na rynku -{" "}
+        <strong>
+          niepowtarzalne szkolenie przeciwzapalne i przeciwstarzeniowe
+        </strong>
+        , które zrewolucjonizuje Twoje podejście do zdrowia!
+      </p>
+      <p>Nawet techniczni Panowie się odnajdują na naszych spotkaniach!</p>
+      {/* video */}
+      <p>Lubimy Was zaskakiwać</p>
+      {/* video */}
+      <p>Poczuj te emocje!</p>
+      {/* video */}
+      <h3>Najważniejsze umiejętności, które wyniesiesz ze szkolenia</h3>
+      <ul className="list">
+        <li className="listItem">
+          <h5>
+            Dowiesz się o różnych stanach zapalnych jako źródłach większości
+            chorób
+          </h5>
+          <p>zrozumiesz przyczyny stanów zapalnych i reakcje organizmu</p>
+        </li>
+        <li className="listItem">
+          <h5>Poznasz diagnostykę stanów zapalnych</h5>
+          <p>
+            odkryjesz wpływ czynników jak: histamina, homocysteina, stan jelit,
+            otyłość, styl życia, choroby autoimmunologiczne na poziom stanów
+            zapalenia w organizmie
+          </p>
+        </li>
+        <li className="listItem">
+          <h5>
+            Zdobędziesz wiedzę o niezbędnych składnikach odżywczych, które
+            hamują starzenie się oraz zabezpieczają przed stanami zapalnymi
+          </h5>
+          <p>
+            nauczysz się tajemnic zdrowego odżywiania, które przyczynią się do
+            redukcji stanów zapalnych w ciele
+          </p>
+        </li>
+        <li className="listItem">
+          <h5>
+            Przyjrzysz się kondycji swoich jelit i żołądka wpływających na Twoje
+            zdrowie fizyczne i psychiczne
+          </h5>
+          <p>dowiesz się, jak poprawić ich funkcjonowanie w dolegliwościach</p>
+        </li>
+        <li className="listItem">
+          <h5>
+            Odkryjesz trzy kluczowe filary zdrowia i ich wpływ na starzenie się
+            i stany zapalne organizmu
+          </h5>
+          <p>zdobędziesz informację o podstawach wspierającego stylu życia</p>
+        </li>
+        <li className="listItem">
+          <h5>Opracujesz swoje sposoby na zwiększenie energii i witalności</h5>
+          <p>określisz nawyki, które pomogą utrzymać młodość na dłużej</p>
+        </li>
+        <li className="listItem">
+          <h5>
+            Nauczysz się jak wykorzystać tętniącą energią moc antyoksydantów
+          </h5>
+          <p>
+            poznasz najlepsze produkty i suplementy, które działają jak eliksir
+            młodości, ożywiając nawet najbardziej zmęczony organizm
+          </p>
+        </li>
+      </ul>
+      <h4>zarezerwuj bilet i dołącz juź dziś!</h4>
+      <p>
+        Będziesz miał/a szansę <strong>współpracować</strong> z praktykującym od
+        kilkunastu lat dietetykiem, psychodietetykiem, coachem i trenerem
+        mentalnym - <strong>Izabelą Potrzebowską-Kniaziuk</strong>. Podzieli się
+        z Tobą doświadczeniem pracy z pacjentami, abyś zyskał/a wiedzę nie tylko
+        teoretyczną, ale również praktyczną
+      </p>
+      <p>
+        Niezbędnym elementem troski o organizm jest odpoczynek i relaks. Na
+        naszym szkoleniu zadba o to <strong>Kasia Koźlik</strong>, która{" "}
+        <strong>w ciekawy i angażujący sposób</strong>
+        przeprowadzi m.in. wizualizacje i zajęcia ruchowe
+      </p>
+      <p>
+        Niezbędnym elementem troski o organizm jest odpoczynek i relaks. Na
+        naszym szkoleniu zadba o to <strong>Kasia Koźlik</strong>, która{" "}
+        <strong>w ciekawy i angażujący sposób</strong>
+        przeprowadzi m.in. wizualizacje i zajęcia ruchowe
+      </p>
+      <h5>Nie bój się!</h5>
+      <p>
+        To nie jest kolejne nudne szkolenie, w którym będziesz tylko słuchać
+        wykładów
+      </p>
+      <p>
+        Nasz program oparty jest na interakcji, praktycznych ćwiczeniach i - jak
+        w szwedzkim bufecie- wybierzesz, to co dla Ciebie najlepsze
+      </p>
+      <p>
+        <strong>
+          Będziesz miał możliwość poznać innych pasjonatów zdrowego stylu życia,
+          którzy, tak jak Ty, pragną przetrwać próbę czasu i cieszyć się życiem
+          pełnym energii
+        </strong>
+      </p>
+      <p>
+        <strong>
+          Niezależnie od tego, czy masz 30, 40, 50 czy 60 lat, to szkolenie
+          pozwoli Ci poczuć się młodo i dynamicznie przez długie lata!
+        </strong>
+      </p>
+      <p>
+        Szkolenie odbędzie się w <strong>wyjątkowym hotelu „Arkas”</strong> w
+        Prószkowie koło Opola. Do dyspozycji uczestników szkolenia hotel oferuje{" "}
+        <strong>strefę spa z saunami, basenem, jacuzzi oraz masażami</strong>
+      </p>
+      <h5>Odprężenie i relaks po zajęciach gwarantowane !</h5>
+      <p>
+        Ponadto hotel Arkas oferuje <strong>świetne jedzenie</strong> o dużej
+        różnorodności. Posiłki starannie przygotowane, bardzo smaczne. Wspólne
+        posiłki będą okazją do nauki dokonywania wyboru wspierającego jedzenia
+      </p>
+      <p>
+        Przestrzeń i wnętrza tego hotelu są specjalnie zorganizowane, aby
+        zapewnić Ci niezapomniane wrażenia i pełen komfort
+      </p>
+      <a href="https://hotelarkas.pl/" target="_blank" rel="noreferrer">
+        Obejrzyj wnętrza
+      </a>
+      <h3>Ale to jeszcze nie koniec</h3>
+      <p>
+        W ramach profilaktyki zdrowotnej będziesz uczestniczyć w zajęciach
+        obejmujących elementy
+        <strong>
+          śmiechoterapii, muzykoterapii, treningów relaksacyjnych i mentalnych
+        </strong>
+        . Poznane metody zastosujesz w codziennej praktyce ograniczania stresu.
+      </p>
+      <p>
+        Weźmiesz udział w błyskawicznym{" "}
+        <strong>kursie pozowania do zdjęć lifestylowych</strong>, aby lepiej się
+        prezentować w mediach społecznościowych lub ofertach biznesowych
+      </p>
+      <h5>I wiele więcej</h5>
+      <ul className="list">
+        <li>
+          <p>
+            <strong>Mini zabieg w mini salonie piękności</strong> - poznasz
+            dobre jakościowo produkty
+          </p>
+        </li>
+        <li>
+          <p>
+            Otrzymasz dostęp na FB do{" "}
+            <strong>naszej ekskluzywnej grupy wsparcia</strong>. Będziesz mógł
+            dzielić się swoimi doświadczeniami z innymi uczestnikami
+          </p>
+        </li>
+        <li>
+          <p>
+            Mamy dla Was jeszcze <strong>bonusy</strong> w postaci prezentacji
+            materiałów ze szkolenia, mini kurs video o pozowaniu do zdjęć, video
+            z afirmacjami i inne o wartości 397 zł !!
+          </p>
+        </li>
+      </ul>
+      <h5>Jeszcze się zastanawiasz?</h5>
+      <a
+        href="https://www.facebook.com/joannacidylofotografiakobieca"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Zerknij na prace Joanny Cidyło
+      </a>
+      <h4>zarezerwuj bilet i dołącz juź dziś!</h4>
+      <h3>Cena regularna od 29.02.2024 to</h3>
+      <p className="price">2540zł</p>
+      <h3>ALE cena w przedsprzedaży to</h3>
+      <p className="price">1850zł</p>
+      <p>+10 % zniżki jeśli przyjedziesz w dwójkę</p>
+      <p>
+        Czy jesteś gotowy, aby przekroczyć próg i zanurzyć się w morzu
+        możliwości ?
+      </p>
+      <p>Jeśli tak, to nie trać ani minuty!</p>
+      <p>
+        <strong>
+          Miejsca na naszym szkoleniu są mocno limitowane, więc zarezerwuj swoje
+          miejsce już teraz!
+        </strong>
+      </p>
+      <h3>
+        Bądź liderem swojego zdrowia i spraw, aby świat oglądał Cię z
+        niedowierzaniem!
+      </h3>
+      <p>
+        Jeszcze nigdy nie było takiego szkolenia, które przyniosłoby tak
+        spektakularne efekty
+      </p>
+      <p>
+        <stron>Dołącz do nas i odmień swoje życie na lepsze!</stron>
+      </p>
+      <h5>
+        Nie czekaj! Wybierz siłę, wybierz zdrowie, wybierz dobre samopoczucie !
+      </h5>
+      {/* fotka opinii o IP-K */}
+      <div>
+        <h5>AGENDA SZKOLENIA</h5>
+        <p>
+          <strong>Piątek - 15:00</strong>
+        </p>
+        <ul className="list">
+          <li className="listItem">
+            <p>zajęcia wstępne zdrowotne,</p>
+          </li>
+          <li className="listItem">
+            <p>śmiechojoga,</p>
+          </li>
+          <li className="listItem">
+            <p>obiadokolacja,</p>
+          </li>
+          <li className="listItem">
+            <p>basen/sauna,</p>
+          </li>
+          <li className="listItem">
+            <p>spotkanie z fotografią i praca ciałem,</p>
+          </li>
+          <li className="listItem">
+            <p>trening mentalny- techniki,</p>
+          </li>
+          <li className="listItem">
+            <p>wieczorny wspólny czas-rozmowy/pytania</p>
+          </li>
+        </ul>
+        <p>
+          <strong>Sobota 8:00</strong>
+        </p>
+        <ul className="list">
+          <li className="listItem">
+            <p>power start,</p>
+          </li>
+          <li className="listItem">
+            <p>śniadanie,</p>
+          </li>
+          <li className="listItem">
+            <p>panel I (Zatrzymaj stany zapalne i popraw zdrowie),</p>
+          </li>
+          <li className="listItem">
+            <p>pytania,</p>
+          </li>
+          <li className="listItem">
+            <p>sesje foto,</p>
+          </li>
+          <li className="listItem">
+            <p>obiad,</p>
+          </li>
+          <li className="listItem">
+            <p>panel II (Młodość na dłużej w naszych komórkach),</p>
+          </li>
+          <li className="listItem">
+            <p>pytania,</p>
+          </li>
+          <li className="listItem">
+            <p>niespodzianka - mini zabieg,</p>
+          </li>
+          <li className="listItem">
+            <p>basen/sauna,</p>
+          </li>
+          <li className="listItem">
+            <p>kolacja,</p>
+          </li>
+          <li className="listItem">
+            <p>wieczór z mentalem,</p>
+          </li>
+          <li className="listItem">
+            <p>zajęcia niespodzianka (będzie angażująco i wesoło)</p>
+          </li>
+        </ul>
+        <p>
+          <strong>Niedziela</strong>
+        </p>
+        <ul className="list">
+          <li className="listItem">
+            <p>8:00 power start,</p>
+          </li>
+          <li className="listItem">
+            <p>śniadanie,</p>
+          </li>
+          <li className="listItem">
+            <p>zajęcia/pytania/podsumowanie,</p>
+          </li>
+          <li className="listItem">
+            <p>12:00 wykwaterowanie z pokoi,</p>
+          </li>
+          <li className="listItem">
+            <p>13:00 dla chętnych wspólne rozmowy, wspólny czas</p>
+          </li>
+        </ul>
+      </div>
+      <h5>
+        eśli pragniesz poprawić swoje zdrowie, energię, a także nauczyć się
+        skutecznych metod radzenia sobie ze swoimi dolegliwościami lub zadziałać
+        profilaktycznie, to nasze szkolenie jest dla Ciebie
+      </h5>
+      <p>
+        Przyjdź i dołącz do naszej społeczności, gdzie będziesz mogła/ł dzielić
+        się doświadczeniami, nawiązywać nowe relacje i czerpać inspirację od
+        innych uczestników
+      </p>
+      <h5>
+        Nie zwlekaj! Teraz jest czas, aby zadbać o swoje zdrowie, samopoczucie i
+        dołączyć do nas na tym niezwykłym szkoleniu
+      </h5>
+      {/** wstawka o bilecie i cenie */}
+      <p>
+        Niezbędna wiedza, relaks, odprężenie, inspirujące rozmowy z uczestnikami
+        i profesjonalistami, a także niesamowite doznania – to wszystko czeka na
+        Ciebie
+      </p>
+      <p>Jeśli potrzebujesz więcej info pisz</p>
+      <h5>dietoteka.rejestracja@gmail.com</h5>
+      <p>
+        <strong>Cena obejmuje:</strong>
+      </p>
+      <ul className="list">
+        <li className="listItem">
+          2 noclegi ( pokoje 2 osobowe)+ posiłki/kawa, herbata,woda przez cały
+          dzień /nauka wyboru jedzenia podczas posiłków,
+        </li>
+        <li className="listItem">
+          Hotel Spa Arkas+ dostęp do basenu, saunarium, jacuzzi, masaże,{" "}
+        </li>
+        <li className="listItem">
+          Zajęcia –panele szkoleniowe, treningi mentalne,
+        </li>
+        <li className="listItem">śmiechojoga,</li>
+        <li className="listItem">trening relaksacyjny - nauka,</li>
+        <li className="listItem">zajęcia ruchowo-relaksacyjne,</li>
+        <li className="listItem">nauka technik,</li>
+        <li className="listItem">
+          zajęcia pozowania, zdjęcia lifestylowe/portrety, niespodzianki-
+          zajęcia np..mini zabieg,
+        </li>
+        <li className="listItem">ale to nie koniec-- RELACJE,</li>
+        <li className="listItem">
+          Bonusy- dodatkowe materiały- video szkolenie +ebook +grupa wsparcia na
+          FB ) o wartości 397 zł (+ 10% zniżki gdy będziesz w dwójkę)
+        </li>
+      </ul>
+      <h3>
+        Zarezerwuj swoje miejsce już teraz i daj szansę sobie na lepsze jutro
+      </h3>
     </div>
   );
 }
