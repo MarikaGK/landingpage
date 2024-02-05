e88e98e07bbd68bcb7a48ee2940f3700f69d7a14
Marika <113508344+MarikaGK@users.noreply.github.com>
2024-02-05 23:04:58 +0100

    styling

---
+++ src/components/BuyTicket.jsx
@@ -0,0 +1,9 @@
+const BuyTicket = () => {
+  return (
+    <>
+      <h4 className="ticket">zarezerwuj bilet i dołącz już dziś!</h4>;
+    </>
+  );
+};
+
+export default BuyTicket;
---
+++ src/components/Pricing.jsx
@@ -0,0 +1,13 @@
+const Pricing = () => {
+  return (
+    <div className="container gap">
+      <h3 className="text-black">Cena regularna od 29.02.2024 to</h3>
+      <p className="price redAccent">2540zł</p>
+      <h3 className="text-black">ALE cena w przedsprzedaży to</h3>
+      <p className="priceGrand greenAccent">1850zł</p>
+      <p className="text-black">+10 % zniżki jeśli przyjedziesz w dwójkę</p>
+    </div>
+  );
+};
+
+export default Pricing;
